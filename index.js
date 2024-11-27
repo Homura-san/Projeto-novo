b6d3e09c635a442c73ec776a88a0c3b5169b4244
Homura-san <erickbispo@ufba.br>
2024-11-27 19:50:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+//const connection = require('./database/database');
+
+// view engine
+app.set('view engine', 'ejs');
+
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render("index");
+})
+
+app.listen(8080, () => {
+    console.log('Servidor rodando...')
+})
